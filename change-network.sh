2ffc3c838e2be899f76d94ae90473debd97eb8d0
William Cabell <william16180@gmail.com>
2019-05-14 19:01:32 -0400

    Fixed wpa command

--- change-network.sh
+++ change-network.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "WARNING: This script will overwrite network.conf. Save the changes elsewhere if you wish to keep them.\n\n"
+echo "WARNING: This script will overwrite network.conf. Save the changes elsewhere if you wish to keep them."
 echo "Enter new network name (SSID): "
 read ssid
-wpa_supplicant $ssid >> ./network.conf
+echo "Enter new network passphrase: "
+read passphrase
+wpa_passphrase $ssid $passphrase >> ./network.conf
 
 # Restart Wi-Fi service
 systemctl reboot connect-to-wifi.service > /dev/null
